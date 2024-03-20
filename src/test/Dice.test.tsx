b4f36f83610dce0b28a233ffe957532427707fb1
josephclander <josephclander@gmail.com>
2024-03-20 09:02:17 +0000

    Clean up test formatting

--- src/test/Dice.test.tsx
+++ src/test/Dice.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import { Dice } from './Dice';
+import { Dice } from '../Dice';
 
 test('Dice value is between 1 and 6 when clicked', () => {
   render(<Dice />);
   const button = screen.getByRole('button');
   const heading = screen.getByRole('heading');
   userEvent.click(button);
-  // needed " heading.textContent ?? '0' " righthandside incase returned null
+  // return value parsing '0' if null
   const diceValue = parseInt(heading.textContent ?? '0', 10);
   // produces number between 1 and 6
   expect(diceValue).toBeGreaterThanOrEqual(1);
